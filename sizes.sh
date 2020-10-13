b9ddff809645e60c67aa11890e47399fcf144559
Laurence Lundblade <lgl@securitytheory.com>
2020-10-12 20:36:45 -0700

    sizes fixes

--- sizes.sh
+++ sizes.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 fun=`nm libt_cose.a | awk '/ [Tt] /{printf "%s|", $3 }'; echo xxxxxxxxxxxxxx`
-nm -r -n -t d -U $1 |\
-awk '{printf "%-32s %4s\n", $3,  offset - $1 }
-     {offset=$1}' |\
-egrep $fun | sort -r -n  -k 2 | tee /tmp/foofoo.txt
 
-cat /tmp/foofoo.txt | awk '{sum += $2} END {print "total                            " sum}'
+nm -n -t d -U $1 |\
+grep ' [Tt] ' |\
+awk 'NR!=1{printf "%-40s %4s\n", name, $1 - offset }
+     {offset=$1; name=$3}' |\
+egrep $fun | sort -r -n  -k 2 | tee /tmp/qsizes.$$
+
+cat /tmp/qsizes.$$ | awk '{sum += $2} END {print "total                            " sum}'
 
 
 
