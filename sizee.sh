7c583af2ee96b29e50d39ee5edeeeb74478dd384
Laurence Lundblade <lgl@securitytheory.com>
2023-08-25 23:19:53 -0700

    Tidy up code size calculators

--- sizee.sh
+++ sizee.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-
-# Print out the sizes of functions in a linked executable that came
-# from libt_cose.a. This doesn't work on ".a" because it gets confused
-# by the separate ".o" files in the ".a". This can work on ".o" files,
-# but only those are to be part of libqcbor.a
-
-fun=`nm libt_cose.a | awk '/ [TtSs] /{printf "%s|", $3 }'; echo xxxxxxxxxxxxxx`
+#
+# This prints out the sizes of *all* the functions in the 
+# file name given. 
+#
+# This doesn't work completely correctly on a ".a" file. It works best
+# on linked executables where the issue of missing one last symbol is
+# usually not an issue. It also works on ".o" files.
 
 # It is not possible to get the size of the last symbol because nm
 # doesn't output the end of the last item in the list.  This is
@@ -17,7 +17,7 @@ nm -n -t d -U $1 |\
 grep ' [TtSs] ' |\
 awk 'NR!=1{printf "%-45s %4s\n", name, $1 - offset }
      {offset=$1; name=$3}' |\
-egrep $fun |\
+grep -v __mh_execute |\
 sort -r -n  -k 2 | tee /tmp/sizex_sh.$$
 
 cat /tmp/sizex_sh.$$ | awk '{sum += $2} END {print "total                                         " sum}'
